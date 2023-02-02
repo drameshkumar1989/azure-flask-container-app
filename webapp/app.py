0ef6290c4c6aa1c8c4ceaf3942dc2459749d00b9
drameshkumar1989 <121951109+drameshkumar1989@users.noreply.github.com>
2023-02-02 09:37:24 +0530

    Update with h2 tag

--- webapp/app.py
+++ webapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def two_hundred():
-    return "<h1>200! from GitHub actions!</h1>"
+    return "<h2>200! from GitHub actions!</h2>"
 
 @app.route("/error")
 def error():
